c00e52273b0f3b4bdedfeac95aa9f98bee64daf9
Gautam Narayan <gautamnarayan@Gautams-MacBook-Pro.local>
2023-10-15 21:55:45 -0400

    adding a comment at the top for the calculator file

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,6 @@
-
+/**
+ * This is the class where we will implement the basic functionalities of a calculator
+ */
 public class Calculator implements CalculatorFunctions {
     // add two numbers
     public int addition(int a, int b) {
