7143ece977f8193d59d7dd4e8f62099e38692d0c
Gautam Narayan <gautamnarayan@Gautams-MacBook-Pro.local>
2023-10-15 21:50:58 -0400

    added a few comments

--- Calculator.java
+++ Calculator.java
@@ -5,21 +5,25 @@ public class Calculator implements CalculatorFunctions {
         return a + b;
     }
 
+    // subtract two numbers
     @Override
     public int subraction(int a, int b) {
         return a - b;
     }
 
+    // multiply two numbers
     @Override
     public int multiply(int a, int b) {
         return a * b;
     }
 
+    // divide the two numbers
     @Override
     public int divide(int a, int b) {
         return a / b;
     }
 
+    // modulus the two numbers
     @Override
     public int modulo(int a, int b) {
         return a % b;
--- Main.java
+++ Main.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
+
+/**
+ * This is the main class where we will run the project
+ */
 public class Main {
     public static void main(String[] args) {
-        // ask for which operation
+        // import the scanner
         Scanner textScan = new Scanner(System.in);
 
         // create our instance
         Calculator c1 = new Calculator();
 
+        // asking for which operation is being used
         System.out.println("Which operation would you like to perform?");
         String myText = textScan.next();
 
+        // ask to enter two numbers
         System.out.println("Enter your two numbers:");
         int a = textScan.nextInt();
         int b = textScan.nextInt();
 
+        // used to determine which operation to perform and print out the result of that
         switch (myText) {
             case "addition":
                 System.out.println(c1.addition(a, b));
@@ -33,6 +40,5 @@ public class Main {
             default:
                 throw new RuntimeException();
         }
-
     }
 }
